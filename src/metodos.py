23246f31ab756a8ebe0483d8243f0a98a473b794
GabrielKishida <gabriel.kishida@usp.br>
2021-03-30 20:02:48 -0300

    :sparkles: Add correct RK4 algorithm

--- src/metodos.py
+++ src/metodos.py
@@ -15,44 +15,31 @@
 
 import numpy as np 
 
-def runge_kutta_4(x0, n, I, f):
-    """
-    x0: valor inicial de x
-    n: quantos pontos teremos no dominio I
-    I: lista do tipo [x0, xf]
-    f: função do tipo f(t, x) callback que com base em valor de t e x, retorna resultado 
-    """
-    t = []
-    x = []
-    x.append(x0)
-    newx = []
-    h = (I[1] - I[0])/n
-    
-    for l in range(n+1):
-        t.append(I[0] + h * l)
+# Brief : Essa funçao aplica o algoritmo Runge Kutta em uma unica iteracao
+def rk4iter(x, t, h, f) :
+    K1 = h*f(t,x)
+    K2 = h*f(t+h/2, x + K1/2)
+    K3 = h*f(t+h/2, x + K2/2)
+    K4 = h*f(t+h, x + K3)
+    return x + (K1 + 2*K2 + 2*K3 + K4)/6
 
-    for l in range(n):
-        for j in range(4):
-            print("l,j ",l,j)
-            #K1 = f(t[l], x[l], j)
-            #K2 = f(t[l] + h/2, x[l] + K1*h/2, j)
-            #K3 = f(t[l] + h/2, x[l] + K2*h/2, j)
-            #K4 = f(t[l] + h, x[l] + K3*h, j)
-            #newx.append(x[l][j] + (K1+2*K2+2*K3+K4)*h/6)
-
-            K1 = f(t[l], x[l], j)
-            x[l][j] += K1*h/2
-            K2 = f(t[l] + h/2, x[l], j)
-            x[l][j] += K2*h/2
-            K3 = f(t[l] + h/2, x[l], j)
-            x[l][j] += K3*h
-            K4 = f(t[l] + h, x[l], j)
-            x[l][j] -= K1*h/2 + K2*h/2 + K3*h
-            newx.append(x[l][j] + (K1+2*K2+2*K3+K4)*h/6)
-            print(newx)
-        x.append(newx) 
-        newx.clear()
-    return np.array(x)
+# Brief : Essa funçao aplica o algoritmo Runge Kutta resolvendo um SPVI
+def rk4system(x0, A, t0, tf, n) :
+    newx = np.zeros(len(A))
+    x = x0.copy()
+    h = (tf-t0)/n
+    t = t0
+    for k in range (1,n+1) :
+        for i in range (len(A)) :
+            coef = A[i][i]
+            sum = 0
+            for j in range (len(A)) :
+                if i != j : 
+                    sum += A[i][j]*x[j]
+            f = lambda t, x : coef*x + sum
+            newx[i] = rk4iter(x[i],t,h,f)
+        x = newx.copy()
+    return x
 
 ##################################################
 #exercicio1.py
@@ -62,15 +49,7 @@ I = [0, 2]
 n = 1
 A = np.array([[-2,-1,-1,-2],[1,-2,2,-1],[-1,-2,-2,-1],[2,-1,1,-2]])
 
-print(np.matmul(A[0],x0))
-
-def f(t, x, i):
-    A = np.array([[-2,-1,-1,-2],[1,-2,2,-1],[-1,-2,-2,-1],[2,-1,1,-2]]) 
-    return np.matmul(A[i],x)
-
-#f = []
-#for i in range(len(A)) :
-#    f.append(lambda t, x: np.matmul(A[i],x))
-    
-resultado = runge_kutta_4(x0, n, I, f)
+resultado = rk4system(x0,A,0,2,1000)
 print(resultado)
+
+
