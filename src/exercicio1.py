1c458520ec03f8ecd2a4872f74fe3fe2927d8993
febagni <febagni@usp.br>
2021-04-02 22:24:56 -0300

    :alembic: Exercise 1 experiments

--- src/exercicio1.py
+++ src/exercicio1.py
@@ -41,7 +41,6 @@ def exercise1_test1():
     '''
     Brief : Essa funçao representa o script para a solucao do exercicio 1 - teste 1
     '''
-
     x0 = [1,1,1,-1]
     
     A = np.array([[-2,-1,-1,-2],
@@ -64,22 +63,41 @@ def exercise1_test1():
         R.append(max(erro)/max(erro_aux))
         plot_scatter(time_array, erro, str("Gráfico de E(t) para n =" + str(n[i])))
         if i == len(n) - 2 : plot_scatter(get_time_array(0,2,n[i+1]), erro_aux, str("Gráfico de E(t) para n =" + str(n[i+1])))
-
     print(R)
 
 def exercise1_test2():
-    u = np.array([-8.79])
+
+    u_0 = np.array([-8.79])
     f = []
     f.append(lambda t,x : 2*t + (x-t*t)*(x-t*t))
 
-    resposta_euler_implicito = implicit_euler_system(u, f, 1.1, 3.0, 5000, 7)
-    print(resposta_euler_implicito[5000])
+    resposta_euler_implicito = implicit_euler_system(u_0, f, 1.1, 3.0, 5000, 7)
+
+    #h = 0.2
+    #x = np.array([1.1, 3.0])
+
+    #[ts, ys] = forwardEuler(f=myFunc, yinit=u_0, x_range=x, h=h)
+
+
+    #--- Calculates the exact solution, for comparison ---#
+    #dt = int((x[-1] - x[0]) / h)
+    #t = [x[0]+i*h for i in range(dt+1)]
+    #yexact = []
+    #for i in range(dt+1):
+    #    ye = t[i]*t[i] + 1/(1-t[i])
+    #    yexact.append(ye)
+
+    #distance_graph(ts, ys, t, yexact, x)
+
+    #Falta cáculo de E_2 e plots 
+
+    print("A solução pelo método de Euler implícito é: ",resposta_euler_implicito[5000])
 
 def main():
     '''
     Brief : Essa funçao eh a main do exercicio 1
     '''
-    #exercise1_test1()
+    exercise1_test1()
     exercise1_test2()
 
 #if __name__ == "__main__":
